9bbc9e34d99bcc6ccc40854136882e373437fc70
shivam21-hub <shivamchowbey.aiml2024@nsec.ac.in>
2025-09-05 07:51:39 +0000

    container config done

---
+++ Basic/Data_Types.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3)
+
+// null is a object data type
+// undefined is a undefined data type
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
