3023277f0f13293ffce547bc3d32d76d292f2c63
shivam21-hub <shivamchowbey.aiml2024@nsec.ac.in>
2025-09-05 12:03:01 +0000

    string

---
+++ Basic/String.js
@@ -0,0 +1,31 @@
+const name = "Shivam"
+const repoCount = 50
+
+// console.log(name + repoCount + " value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Shivam-chowbey')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.indexOf('h')) // 1
+
+// console.log(gameName.substring(0,4)) // Shiv
+
+// console.log(gameName.slice(-6, 6))
+
+// const str = "     Shivam    "
+// console.log(str);
+
+// console.log(str.trim()) 
+
+
+const str = "https.//Shivam.com/Shivam2%Chowbey"
+// console.log(str.replace("2%",'-'))
+
+console.log(gameName.split('-'))
