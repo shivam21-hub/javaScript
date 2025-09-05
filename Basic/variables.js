416beda6098bb1f4bd45a131bbab3e00021ad23b
shivam21-hub <shivamchowbey.aiml2024@nsec.ac.in>
2025-09-05 07:34:24 +0000

    conatiner config done

---
+++ Basic/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144534
+let accountEmail = "Shivamchowbey21@gmail.com"
+var accountPassword = "shivam"
+acountCity = "Kolkata"
+
+// accountId = 3245
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,acountCity])
