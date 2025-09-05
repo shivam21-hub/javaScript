f95e0840a52680ce780ccfb78162df1e8c82ff3b
shivam21-hub <shivamchowbey.aiml2024@nsec.ac.in>
2025-09-05 08:42:48 +0000

    container config done

--- Basic/Conversion_DataTypes.js
+++ Basic/Conversion_DataTypes.js
@@ -1,8 +1,20 @@
 let age = "25"
 
-console.log(typeof age)
+// console.log(typeof age)
 
-let valueInNumber = Number(age)
+// let valueInNumber = Number(age)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
+//********** OPERATION *******/
+//  let value = 33
+//  let negValue = -value
+//  console.log(negValue)
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+console.log(+true) // 1
+console.log(+"") // 0
