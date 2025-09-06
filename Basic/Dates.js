a5eb002ba6e260175a465bdf8c91b74265b657ab
shivam21-hub <shivamchowbey.aiml2024@nsec.ac.in>
2025-09-06 03:26:14 +0000

    Dates

---
+++ Basic/Dates.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+console.log(myDate) // 2025-09-06T03:19:19.972Z
+console.log(myDate.toString()); // Sat Sep 06 2025 03:20:18 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()); // Sat Sep 06 2025 
+console.log(myDate.toISOString()) // 2025-09-06T03:21:26.118Z
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleString()); // 9/6/2025, 3:24:24 AM
+console.log(myDate.toLocaleDateString()); // 9/6/2025
+
+console.log(typeof myDate) // object
+console.log(myDate.)
+
+
